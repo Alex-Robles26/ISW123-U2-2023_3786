14eda70dc6db3a4b0a05f02dad1d736d15c9c615
Alex Robles <alexroblescarpioz@gmail.com>
2026-01-24 18:50:20 -0400

    async y evento 2023-3786

--- Delivery/Program.cs
+++ Delivery/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace ISW123_2023_3786
 {
-    // Excepción personalizada
     public class PizzaNoDisponibleException_3786 : Exception
     {
         public PizzaNoDisponibleException_3786(string mensaje) : base(mensaje)
@@ -10,13 +10,35 @@ namespace ISW123_2023_3786
         }
     }
 
+    class Cocina
+    {
+        //  Evento
+        public event Action<string> PizzaLista;
+
+        //  Método async
+        public async Task PrepararPizzaAsync(string tipo)
+        {
+            await Task.Delay(4300);
+            PizzaLista?.Invoke($" Pizza {tipo} lista para delivery");
+        }
+    }
+
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             Console.WriteLine("Nombre: Manaury Roble");
             Console.WriteLine("Matrícula: 2023-3786");
-            Console.ReadKey();
+
+            Cocina cocina = new Cocina();
+
+            //  Suscriptor
+            cocina.PizzaLista += mensaje =>
+            {
+                Console.WriteLine(mensaje);
+            };
+
+            await cocina.PrepararPizzaAsync("Pepperoni");
         }
     }
 }
