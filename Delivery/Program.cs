71320d4f2df2965b6be88f7a9ea7a21a3c7c17f8
Alex Robles <alexroblescarpioz@gmail.com>
2026-01-24 18:44:44 -0400

    Agregar archivos de proyecto.

---
+++ Delivery/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ISW123_2023_3786
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nombre: Manaury Robles");
+            Console.WriteLine("Matrícula: 2023-3786");
+            Console.ReadKey();
+        }
+    }
+}
