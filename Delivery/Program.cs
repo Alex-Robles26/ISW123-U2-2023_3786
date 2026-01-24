60d440737652c28ce8d53b14bd8a33c81d123fa5
Alex Robles <alexroblescarpioz@gmail.com>
2026-01-24 18:47:11 -0400

    excepcion 2023-3786

--- Delivery/Program.cs
+++ Delivery/Program.cs
@@ -2,6 +2,14 @@
 
 namespace ISW123_2023_3786
 {
+    // Excepción personalizada
+    public class PizzaNoDisponibleException_3786 : Exception
+    {
+        public PizzaNoDisponibleException_3786(string mensaje) : base(mensaje)
+        {
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
